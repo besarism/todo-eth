e2cd85ee4ebe281457efd75f815fb282bcec9286
besarism <b3sar11@gmail.com>
2022-01-09 01:06:31 +0100

    toggle task add and trigger events

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -11,6 +11,18 @@ contract TodoList {
 
     mapping(uint => Task) public tasks;
 
+    event TaskCreated(
+        uint id,
+        string content,
+        bool completed
+    );
+
+    event TaskCompleted(
+        uint id,
+        bool completed
+    );
+
+
     constructor() public {
         createTask("Make a dapp");
     }
@@ -18,5 +30,13 @@ contract TodoList {
     function createTask(string memory _content) public {
         taskCount++;
         tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
+    }
+
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
     }
 }
